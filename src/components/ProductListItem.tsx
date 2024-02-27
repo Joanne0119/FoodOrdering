0c4aae3b14e7ca08960a80f0b290c0098210c89c
Joanne0119 <liuchengen52033@gmail.com>
2024-02-27 19:59:17 +0800

    add type for component props to fix error

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,12 +1,19 @@
 import { StyleSheet ,Text, View, Image } from 'react-native';
 import Colors from '../constants/Colors';
 import products from '../../assets/data/products';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Product } from '../types';
 
-const ProductListItem = ({ product }) => {
+export const defaultImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
+
+type ProductListItemProps = { 
+  //typescript need to make sure the data type
+  product: Product
+}
+
+const ProductListItem = ({ product } : ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: product.image }} />
+      <Image style={styles.image} source={{ uri: product.image || defaultImage}} />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
